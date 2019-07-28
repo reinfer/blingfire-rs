959928fcf8fbe31f6121eca2f59a2b60f05dca2d
Marius Cobzarenco <marius@reinfer.io>
2019-07-28 21:01:50 +0100

    Add documentation

--- blingfire/src/errors.rs
+++ blingfire/src/errors.rs
@@ -1,16 +1,20 @@
 use snafu::Snafu;
 use std::result::Result as StdResult;
 
+/// Error enum encoding tokenization errors.
 #[derive(Debug, Snafu, PartialEq)]
 #[snafu(visibility = "pub")]
 pub enum Error {
-    #[snafu(display("Source buffer is too large (capacity > 2^32)."))]
-    SourceTooLarge,
+    /// Source buffer is too large (capacity > MAX_TEXT_LENGTH).
+    #[snafu(display("Source buffer is too large (capacity > {}).", max_text_length))]
+    SourceTooLarge { max_text_length: usize },
 
+    /// An unknown error caused the tokenizer to fail (the C++ function returned -1).
     #[snafu(display(
-        "An unknown error caused the tokenizer to fail (the C function returned -1)."
+        "An unknown error caused the tokenizer to fail (the C++ function returned -1)."
     ))]
     UnknownError,
 }
 
+/// Result of calling the tokenizer functions.
 pub type Result<T, E = Error> = StdResult<T, E>;
